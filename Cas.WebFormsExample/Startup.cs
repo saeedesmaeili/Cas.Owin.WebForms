ab49eee70a0de0e01e91fed6f02b6124c9063e79
SEsmaeili <SEsmaeili@mafpars.com>
2021-09-30 00:44:33 +0330

    Add project files.

---
+++ Cas.WebFormsExample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cas.WebFormsExample.Startup))]
+namespace Cas.WebFormsExample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
